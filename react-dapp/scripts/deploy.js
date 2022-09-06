5828294b82463fcb47cd59d6b06073a653ac195e
OmarMWarraich <earthling986@gmail.com>
2022-09-07 00:11:38 +0500

    feat: Add Token.sol and ui functionality

--- react-dapp/scripts/deploy.js
+++ react-dapp/scripts/deploy.js
@@ -7,16 +7,23 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const [deployer] = await hre.ethers.getSigners();
 
-  await greeter.deployed();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+const Greeter = await hre.ethers.getContractFactory("Greeter");
+const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+const Token = await hre.ethers.getContractFactory("Token");
+const token = await Token.deploy();
+
+await greeter.deployed();
+await token.deployed();
+
+console.log("Greeter deployed to:", greeter.address);
+console.log("Token deployed to:", token.address);
+
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import Token from './artifacts/contracts/Token.sol/Token.json';
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 
 function App() {
   // store greeting in local state
   const [greeting, setGreetingValue] = useState();
+  const [userAccount, setUserAccount] = useState();
+  const [amount, setAmount] = useState(0);
 
   // request access to the user's MetaMask account
   async function requestAccount() {
@@ -19,6 +23,7 @@ function App() {
   async function fetchGreeting() {
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider });
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
       try {
         const data = await contract.greet();
@@ -26,6 +31,16 @@ function App() {
       } catch (err) {
         console.log("Error: ", err);
       }
+    }
+  }
+
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
     }
   }
 
@@ -43,12 +58,30 @@ function App() {
     }
   }
 
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transaction = await contract.transfer(userAccount, amount);
+      await transaction.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={fetchGreeting}>Fetch Greeting</button>
         <button onClick={setGreeting}>Set Greeting</button>
         <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+
+        <br />
+        <button onClick={getBalance}>Get Balance</button>
+        <button onClick={sendCoins}>Send Coins</button>
+        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
       </header>
     </div>
   );
